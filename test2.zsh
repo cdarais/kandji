1564242ac7bbea12970cc06919c3ebe081695ab5
Chris Darais <chrisdarais@icloud.com>
2022-05-05 11:33:48 -0600

    update

--- test2.zsh
+++ test2.zsh
@@ -1,8 +1,13 @@
 
 declare -a fonts 
 
+library1="/Library/Fonts"
+library2="/System/Library/Fonts"
+library3="$library2/Supplemental"
+library4="~/$library1"
+
 fonts=(
-    "/Library/Fonts/Inter-Black.otf"
+    "Inter-Black.otf"
     "Inter-ExtraBold.otf"
     "Inter-Italic.otf"
     "Inter-MediumItalic.otf"
@@ -24,7 +29,7 @@ fonts=(
 )
 
 for f in "${fonts[@]}"; do
-    if [[ -f "$f" ]]; then
-        echo "$f exists"
+    if [[ ! -f "$library1/$f" && ! -f "$library2/$f" && ! -f "$library3/$f" && ! -f "$library4/$f" ]]; then
+        echo "$f not found"
     fi
 done
