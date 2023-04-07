05de4080e9884b0f44471edd0cf21224eeef9580
Chris Darais <chrisdarais@icloud.com>
2023-04-07 16:38:23 -0600

    stringify

--- cleanDock/scripts/functions.ps1
+++ cleanDock/scripts/functions.ps1
@@ -11,7 +11,7 @@ function addAppItem {
 
 	$item.Add("<dict><key>tile-data</key><dict><key>file-data</key><dict>")
 	$item.Add("<key>_CFURLString</key><string>")
-	$item.Add($itemName)
+	$item.Add("$itemName")
 	$item.Add("</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>")
 
 	defaults write $dock persistent-apps -array-add $item
@@ -29,7 +29,7 @@ function addOtherItem {
 
 	$item.Add("<dict><key>tile-data</key><dict><key>file-data</key><dict>")
 	$item.Add("<key>_CFURLString</key><string>")
-	$item.Add($itemName)
+	$item.Add("$itemName")
 	$item.Add("</string><key>_CFURLStringType</key><integer>0</integer></dict>")
 	$item.Add("<key>file-label</key><string>Downloads</string><key>file-type</key><string>2</string></dict>")
 	$item.Add("<key>tile-type</key><string>directory-tile</string></dict>")
