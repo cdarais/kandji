47df0771b5acafabb0b718d0d1636176749791f2
Chris Darais <chrisdarais@icloud.com>
2023-04-07 12:10:40 -0600

    final fonts

--- fonts/scripts/Remediate-Fonts.ps1
+++ fonts/scripts/Remediate-Fonts.ps1
@@ -3,13 +3,8 @@
 
 $uri = "$($args[1])/data"
 
-$prevProgressPreference = $global:ProgressPreference
-$global:ProgressPreference = 'SilentlyContinue'
-
 Write-Host "installing missing fonts"
 foreach ($missingFont in checkForFonts) {
 	Write-Host "installing $missingFont"
 	Invoke-WebRequest -Uri "$uri/$missingFont" -OutFile "$($libraries[0])/$missingFont"
 }
-
-$global:ProgressPreference = $prevProgressPreference
