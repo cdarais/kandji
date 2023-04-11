0ba387000d56268a78b06f55ab6e5f10e442fe26
Chris Darais <chrisdarais@icloud.com>
2023-04-11 15:14:11 -0600

    final

--- cleanDock/scripts/variables.zsh
+++ cleanDock/scripts/variables.zsh
@@ -2,19 +2,18 @@
 
 currentUser=$(echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }')
 uid=$(id -u "$currentUser")
-downloadFolder=/Users/$currentUser/Downloads
 
 declare -a dockApps=(
 	"/Applications/Microsoft Edge.app"
 	"/System/Applications/Messages.app"
 	"/System/Applications/Mail.app"
 	"/System/Applications/Calendar.app"
+	"/Applications/Microsoft Outlook.app"
 	"/Applications/Microsoft Teams.app"
 	"/Applications/Slack.app"
 	"/Applications/zoom.us.app"
 	"/System/Applications/TV.app"
 	"/System/Applications/Music.app"
 	"/Applications/Visual Studio Code.app"
+	"/Applications/Kandji Self Service.app"
 )
-
-persistentApp="<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$i</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
