2958f6fd841fce5f36e2b5f9ccab0a8043b07a19
Chris Darais <chrisdarais@icloud.com>
2023-04-11 15:02:27 -0600

    what in the eff

--- cleanDock/scripts/functions.zsh
+++ cleanDock/scripts/functions.zsh
@@ -12,6 +12,7 @@ waitForDesktop () {
 runAsUser (){
 	if [[ "$currentUser" != "loginwindow" ]]
 	then
+		echo "running as $currentUser"
 		launchctl asuser "$uid" "$@"
 	else
 		echo "no user logged in"
