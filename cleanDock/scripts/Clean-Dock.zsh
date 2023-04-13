41986e9deafce7f0e1adc904160f6bc481e46b19
Chris Darais <chrisdarais@icloud.com>
2023-04-13 12:44:27 -0600

    new dock clean

--- cleanDock/scripts/Clean-Dock.zsh
+++ cleanDock/scripts/Clean-Dock.zsh
@@ -9,10 +9,10 @@ then
 	waitForDesktop
 
 	echo "Disable show recent items"
-	runAsUser /usr/bin/defaults write com.apple.dock "show-recents" -bool "false"
+	runAsUser /usr/bin/defaults write com.apple.dock show-recents -bool "false"
 
 	echo "Enable Minimize Icons into Dock Icons"
-	runAsUser /usr/bin/defaults write com.apple.dock "minimize-to-application" -bool TRUE
+	runAsUser /usr/bin/defaults write com.apple.dock minimize-to-application -bool TRUE
 
 	echo "Removing Dock Persistent Apps"
 	runAsUser /usr/local/bin/dockutil --remove all --no-restart --allhomes
@@ -25,7 +25,20 @@ then
 
 	echo "Adding Downloads Stack"
 	runAsUser /usr/local/bin/dockutil --add '~/Downloads' --view auto --display stack --allhomes --no-restart
+		
+	echo "Showing finder path bar"
+	runAsUser /usr/bin/defaults write com.apple.finder ShowPathbar -bool "true"
+
+	echo "Changing default view"
+	runAsUser /usr/bin/defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+
+	echo "Setting up trash purge"
+	runAsUser /usr/bin/defaults write com.apple.finder FXRemoveOldTrashItems -bool "true"
 	
 	echo "Restarting Dock"
 	/usr/bin/killall Dock
+
+	echo "Restarting Finder"
+	/usr/bin/killall Finder
+
 fi
