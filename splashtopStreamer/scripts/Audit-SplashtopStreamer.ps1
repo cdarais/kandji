4909f1c5cdd1a507f54bf2863841ef3cd2bc9986
Chris Darais <chrisdarais@icloud.com>
2023-04-24 14:59:38 -0600

    stupid variable

--- splashtopStreamer/scripts/Audit-SplashtopStreamer.ps1
+++ splashtopStreamer/scripts/Audit-SplashtopStreamer.ps1
@@ -1,8 +1,11 @@
 . "$($args[0])/functions.ps1"
 . "$($args[0])/variables.ps1"
 
+Write-Output "$($args[0])/$auditFile"
+$scriptCommand = "$($args[0])/$auditFile '-profileName ""$profileName"" -appName ""$appName"" -appVersion ""$appVersion""'"
+
 Write-Output "checking data"
-if (Invoke-Expression "$($args[0])/$auditFile '-profileName ""$profileName"" -appName ""$appName"" -appVersion ""$appVersion""'") {
+if (Invoke-Expression -Command $scriptCommand ) {
 	Write-Host "discrepency found"
 	New-Item -Path "$($args[0])/1" -ItemType File | Out-Null
 } else {
