517e6009e9d80eaf907f5819ac9a512a31d0dd98
Chris Darais <chrisdarais@icloud.com>
2023-04-24 14:26:16 -0600

    minor update

--- splashtopStreamer/scripts/Audit-SplashtopStreamer.ps1
+++ splashtopStreamer/scripts/Audit-SplashtopStreamer.ps1
@@ -3,8 +3,8 @@
 
 Write-Output "checking data"
 if (Invoke-Expression """$($args[0])/$auditFile"" -profileName ""$profileName"" -appName ""$appName"" -appVersion ""$appVersion""") {
-	Write-Host "no discrepency found"
-} else {
 	Write-Host "discrepency found"
 	New-Item -Path "$($args[0])/1" -ItemType File | Out-Null
+} else {
+	Write-Host "no discrepency found"
 }
