b17320f41cdb6964315dccfab8d7ed06055d4cf5
Chris Darais <chrisdarais@icloud.com>
2023-05-02 09:35:58 -0600

    dumb

--- googleDriveFiles/drivers/audit.zsh
+++ googleDriveFiles/drivers/audit.zsh
@@ -9,12 +9,20 @@ files=(
 	"Audit-GoogleDriveFiles.ps1"
 	"functions.ps1"
 	"variables.ps1"
-	"functions.zsh"
-	"variables.zsh"
 )
 
-source $baseDirectory/variables.zsh
-source $baseDirectory/functions.zsh
+currentUser=$(echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }')
+uid=$(id -u "$currentUser")
+
+runAsUser () {
+	if [[ "$currentUser" != "loginwindow" ]]
+	then
+		echo "running as $currentUser"
+		launchctl asuser "$uid" "$@"
+	else
+		echo "no user logged in"
+	fi
+}
 
 if [[ -e /usr/local/bin/pwsh ]]
 then
