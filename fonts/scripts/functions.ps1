81f62a4b40d3478e7a0c3e5041a682e1409f9fb5
Chris Darais <chrisdarais@icloud.com>
2023-05-03 14:27:49 -0600

    dumb

--- fonts/scripts/functions.ps1
+++ fonts/scripts/functions.ps1
@@ -2,10 +2,10 @@ function checkForFonts {
 	param (
 		$FontsToCheck
 	)
-	
+
 	$missingFonts = [System.Collections.ArrayList]::new()
 
-	foreach ($font in $fontsToCheck) {
+	foreach ($font in $FontsToCheck) {
 		
 		if ( -not ($libraries | Where-Object { Test-Path -Path "$_/$font" -PathType Leaf }) ) {
 			$missingFonts.Add($font)
