462046c54e2e01d5786f36e3fe69bdd10abe3d8b
Chris Darais <chris.darais@icloud.com>
2023-07-20 16:10:15 -0600

    company portal

---
+++ companyPortal/scripts/Remediate-CompanyPortal.ps1
@@ -0,0 +1,8 @@
+. "$($args[0])/functions.ps1"
+. "$($args[0])/variables.ps1"
+
+
+Write-Host "attempting to resolve discrepencies"
+
+
+Invoke-Expression "$($args[0])/Audit-CompanyPortal.ps1 $($args[0])"
---
+++ companyPortal/scripts/variables.ps1
@@ -0,0 +1,10 @@
+$auditFile = "Get-AppAudit.ps1"
+$appName = "Company Portal.app"
+$profileName = $null
+$appVersion = "5.2307.1"
+
+Write-Output $auditFile | Out-Null
+Write-Output $appName | Out-Null
+Write-Output $profileName | Out-Null
+Write-Output $appVersion | Out-Null
+Write-Output $streamerCode | Out-Null
