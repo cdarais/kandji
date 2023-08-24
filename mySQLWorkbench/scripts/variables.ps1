992a2c7c235ba34358821aee0e38a656b8fbe6b7
Chris Darais <chris.darais@icloud.com>
2023-08-24 15:31:33 -0600

    mySQLWorkbench

---
+++ mySQLWorkbench/scripts/variables.ps1
@@ -0,0 +1,10 @@
+$auditFile = "Get-AppAudit.ps1"
+$appName = "MySQL Workbench.app"
+$profileName = $null
+$appVersion = "8.0.34.CE"
+
+Write-Output $auditFile | Out-Null
+Write-Output $appName | Out-Null
+Write-Output $profileName | Out-Null
+Write-Output $appVersion | Out-Null
+Write-Output $streamerCode | Out-Null
