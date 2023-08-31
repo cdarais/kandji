64b8e5eb096b204dbff3d244039eef99bde32140
Chris Darais <chris.darais@icloud.com>
2023-08-30 18:42:41 -0600

    break loop for not logged in user

--- systemSettings/scripts/functions.zsh
+++ systemSettings/scripts/functions.zsh
@@ -1,12 +1,23 @@
 waitForDesktop () {
+	count=0
 	until ps aux | grep /System/Library/CoreServices/Dock.app/Contents/MacOS/Dock | grep -v grep &>/dev/null
 	do
-		delay=$(( $RANDOM % 50 + 10 ))
+		delay=$(( $RANDOM % 30 + 5 ))
 		echo " + Dock not running, waiting [$delay] seconds"
 		sleep $delay
+		if [ $count -eq 3 ]
+		then
+			break
+		fi
+		((count++))
 	done
 	
-	echo "Dock is here, lets carry on"
+	if [ $count -ne 0 ]
+	then
+		echo "User not logged in"
+	else
+		echo "Dock is here, lets carry on"
+	fi
 }
 
 runAsUser () {
