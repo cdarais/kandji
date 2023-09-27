e4abd14a2cae6c29e1d1dc74024592384a3265ca
Chris Darais <chris.darais@icloud.com>
2023-09-27 16:03:13 -0600

    wrong write

--- xcodeCLTools/scripts/functions.ps1
+++ xcodeCLTools/scripts/functions.ps1
@@ -3,7 +3,7 @@ function checkXcode{
 		$version
 	)
 	$installedVersion = pkgutil --pkg-info com.apple.pkg.CLTools_Executables | awk '/version: / {print $NF }' | cut -d. -f-2
-	Write-Output "expected to find ""$version"" and found ""$installedVersion"""
+	Write-Host "expected to find ""$version"" and found ""$installedVersion"""
 	if ([decimal]$installedVersion -ge [decimal]$version ) {
 		return $true
 	}
