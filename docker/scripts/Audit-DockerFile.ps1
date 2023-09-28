a4721f451be9bc68b6c6ee5bd88978fb68e040de
Chris Darais <chris.darais@icloud.com>
2023-09-28 17:27:29 -0600

    fix compare output

--- docker/scripts/Audit-DockerFile.ps1
+++ docker/scripts/Audit-DockerFile.ps1
@@ -2,10 +2,11 @@
 . "$($args[0])/variables.ps1"
 
 Write-Host "Comparing Data"
+Write-Host "expected ""$defaultDockerFile"" and found ""$($currentDockerFile)"""
+
 if (compareDockerFiles -currentData $currentDockerFile -defaultData $defaultDockerFile) {
 	Write-Host "no discrepency found"
 } else {
 	Write-Host "discrepency found"
-	Write-Host "expected ""$defaultDockerFile"" and found ""$($currentDockerFile.allowedOrgs)"""
 	New-Item -Path "$($args[0])/1" -ItemType File | Out-Null
 }
