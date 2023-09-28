37341647a104a04f7138bed28561e07402b30d8a
Chris Darais <chris.darais@icloud.com>
2023-09-28 16:47:58 -0600

    fix dockerfile compare

--- docker/scripts/variables.ps1
+++ docker/scripts/variables.ps1
@@ -1,7 +1,7 @@
 $dockerFile = "/Library/Application Support/com.docker.docker/registry.json"
 
 $currentDockerFile = (Get-Content -Path $dockerFile) | ConvertFrom-Json
-$defaultDockerFile = '{"allowedOrgs":["workboardinc"]}' | ConvertFrom-Json
+$defaultDockerFile = "workboardinc"
 
 Write-Output $dockerFile | Out-Null
 Write-Output $defaultDockerFile | Out-Null
