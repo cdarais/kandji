166882ddf80dcbe8a4096fa0e1eda73055b446f7
Chris Darais <chris.darais@icloud.com>
2023-09-29 10:57:25 -0600

    check if installed

--- docker/scripts/Audit-DockerFile.ps1
+++ docker/scripts/Audit-DockerFile.ps1
@@ -4,6 +4,8 @@
 Write-Host "Comparing Data"
 Write-Host "expected ""$defaultDockerFile"" and found ""$($currentDockerFile)"""
 
+if (-not (Test-Path -Path "/Applications/Docker.app" )) { return }
+
 if (compareDockerFiles -currentData $currentDockerFile -defaultData $defaultDockerFile) {
 	Write-Host "no discrepency found"
 } else {
