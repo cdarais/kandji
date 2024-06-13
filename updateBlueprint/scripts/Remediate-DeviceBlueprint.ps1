56a2c00642633a8dc20ae1bae743414fcfa92e6f
Chris Darais <chrisdarais@icloud.com>
2024-06-13 11:36:03 -0600

    remove old

--- updateBlueprint/scripts/Remediate-DeviceBlueprint.ps1
+++
@@ -1,9 +0,0 @@
-. "$($args[0])/functions.ps1"
-. "$($args[0])/variables.ps1"
-
-
-Write-Host "attempting to resolve discrepencies"
-
-updateDevice -token $args[1] -serial $serial
-
-Invoke-Expression "$($args[0])/Audit-DeviceBlueprint.ps1 $($args[0]) $($args[1])"
