f1469a1cc4268abdcfa92ca0cb4a6abcd0e13aaa
Chris Darais <chrisdarais@icloud.com>
2024-06-13 12:15:07 -0600

    try this

--- deviceBlueprint/scripts/variables.ps1
+++ deviceBlueprint/scripts/variables.ps1
@@ -2,7 +2,7 @@ $auditFile = "Get-AppAudit.ps1"
 $onboardingBlueprint = "85335437-d32a-4758-8961-ec83332494e2"
 $newBlueprint = "baa9c249-69d7-48f8-b35d-ff4f89c2b6e8"
 $apiURL = "https://workboard.api.kandji.io/api/v1"
-$serial = (system_profiler SPHardwareDataType | grep Serial) | Out-Null
+$serial = (system_profiler SPHardwareDataType | grep Serial)
 $serial = $serial.Substring($serial.IndexOf(":") + 1).Trim()
 
 
