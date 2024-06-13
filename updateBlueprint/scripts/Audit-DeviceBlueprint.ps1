c99e73dd9ead9a532db7bd019a372dc0ffbc9c15
Chris Darais <chrisdarais@icloud.com>
2024-06-13 11:35:47 -0600

    deviceBlueprint

---
+++ updateBlueprint/scripts/Audit-DeviceBlueprint.ps1
@@ -0,0 +1,10 @@
+. "$($args[0])/functions.ps1"
+. "$($args[0])/variables.ps1"
+
+Write-Output "checking data"
+if ((getDevice -token $args[1] -serial $serial).blueprint_id -eq $newBlueprint) {
+	Write-Host "no discrepency found"
+} else {
+	Write-Host "discrepency found"
+	New-Item -Path "$($args[0])/1" -ItemType File | Out-Null
+}
