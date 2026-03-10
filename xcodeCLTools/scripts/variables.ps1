6fed48e3ca707fc813e260427cd27b28edf0380c
cdarais <chrisdarais@icloud.com>
2026-03-10 20:25:49 +0000

    ado xcode cltools update

---
+++ xcodeCLTools/scripts/variables.ps1
@@ -0,0 +1,9 @@
+$auditFile = "Get-AppAudit.ps1"
+$appName = $null
+$profileName = $null
+$appVersion = "26.3"
+
+Write-Output $auditFile | Out-Null
+Write-Output $appName | Out-Null
+Write-Output $profileName | Out-Null
+Write-Output $appVersion | Out-Null
